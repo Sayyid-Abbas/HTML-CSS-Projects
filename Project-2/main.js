a8afdc3584ed06638bdef6606d4097a30b20c38c
Sayyid Abbas <sayyid.abbas.ar@gmail.com>
2025-07-15 17:40:22 +0300

    Fixing the toggle links for phones, implementing some javaScript for Portfolio section to enable the filter, making the textarea not resizeable in the contact section.

--- Project-2/main.js
+++ Project-2/main.js
@@ -1,3 +1,17 @@
+// Links menu
+
+let bars = document.querySelector(".bars");
+let links = document.querySelector("nav ul");
+bars.onclick = function() {
+    bars.classList.toggle("show");
+}
+
+document.addEventListener("click", (e) => {
+    if(!bars.contains(e.target) && !links.contains(e.target)) {
+        bars.classList.remove("show");
+    }
+});
+
 // A button to scroll up
 
 let myButton = document.querySelector(".btn");
@@ -9,3 +23,23 @@ window.onscroll = function() {
     }
 }
 myButton.onclick = () => window.scrollTo({top: 0, behavior: "smooth"});
+
+// Portfolio Section
+
+let selectors = document.querySelectorAll(".portfolio ul li");
+document.addEventListener("click", (e) => {
+    selectors.forEach((select) => {
+        if(select.contains(e.target)) {
+            selectors.forEach(s => s.classList.remove("active"));
+            select.classList.add("active");
+            showData(select.dataset.kind);
+        }
+    });
+});
+
+function showData(className) {
+    document.querySelectorAll(".portfolio .image-container > div").forEach((li) => {
+        li.style.display = "none";
+    });
+    document.querySelectorAll(className).forEach(li => li.style.display = "block");
+};
